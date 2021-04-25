b55603f97b2cca2cde912eaacd41596f6ae26cfc
サム <>
2021-04-25 21:24:51 +0200

    working out kinks in book model

--- Ingester/src/main/java/Ingester.java
+++ Ingester/src/main/java/Ingester.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
 import com.fasterxml.jackson.databind.introspect.AnnotatedParameter;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import data.Author;
+import data.Book;
 
 import java.io.IOException;
 
@@ -36,11 +37,23 @@ public class Ingester {
             return super.findImplicitPropertyName(m);
         }
     };
+
     public static void main(String[] args) {
         System.err.println("Hello world");
         ObjectMapper mapper = new ObjectMapper();
         mapper.setAnnotationIntrospector(Ingester.implicitRecordAI);
-        String test = """
+
+        try {
+            Author auth = mapper.readValue(test, Author.class);
+            Book book = mapper.readValue(test2, Book.class);
+            System.err.println("hello");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static String test = """
                 {
                     "url": "https://www.goodreads.com/author/show/1476.Doris_Kearns_Goodwin",
                     "name": "Doris Kearns Goodwin",
@@ -55,12 +68,60 @@ public class Ingester {
                     "num_ratings": 254438,
                     "about": "test"
                 }""";
-        try {
-            Author auth = mapper.readValue(test, Author.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
-    }
+    static String test2 = """
+                {
+                    "url": "https://www.goodreads.com/book/show/2199.Team_of_Rivals",
+                    "title": "Team of Rivals: The Political Genius of Abraham Lincoln",
+                    "author": "Doris Kearns Goodwin",
+                    "num_ratings": 164350,
+                    "num_reviews": 7315,
+                    "avg_rating": 4.24,
+                    "num_pages": 916,
+                    "language": "English",
+                    "publish_date": "2006-09-26 00:00:00",
+                    "original_publish_year": 2005,
+                    "genres": [
+                        "North American Hi...",
+                        "American History",
+                        "History",
+                        "Audiobook",
+                        "Historical",
+                        "Civil War",
+                        "Biography",
+                        "Presidents",
+                        "Leadership",
+                        "Politics",
+                        "Military History",
+                        "Nonfiction"
+                    ],
+                    "awards": [
+                        "Benjamin Barondess Award (2006)",
+                        "Lincoln Prize (2006)",
+                        "National Book Critics Circle Award Nominee for Biography (2005)",
+                        "New-York Historical Society American History Book Prize"
+                    ],
+                    "characters": [
+                        "Abraham Lincoln",
+                        "William H. Seward",
+                        "Edwin McMasters Stanton",
+                        "Salmon P. Chase",
+                        "Edward Bates"
+                    ],
+                    "places": [
+                        "Springfield, Illinois",
+                        "Chicago, Illinois",
+                        "Illinois"
+                    ],
+                    "isbn": "0743270754",
+                    "isbn13": "9780743270755",
+                    "rating_histogram": {
+                        "5": 89209,
+                        "4": 44984,
+                        "3": 17493,
+                        "2": 6235,
+                        "1": 6429
+                    }
+                    }""";
 
 }
